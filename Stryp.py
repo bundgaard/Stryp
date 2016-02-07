0016459edb98645c040a58f78943258fbcd6aca5
David <dapoman@gmail.com>
2016-02-07 19:02:11 +0100

    First draft

---
+++ Stryp.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import requests
+import json
+import os
+import sys
+__version__ = 1.0
+
+
+def download_restriction():
+    r = requests.get("")
+
+
+if __name__ == '__main__':
+    print "Stryp (C) Version: {}".format(__version__)
